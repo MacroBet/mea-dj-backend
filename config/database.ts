c857b142e2b907c3a725362424916cccee2bc528
Marco Bellia <ing.bellia@gmail.com>
2023-10-27 16:54:41 +0200

    fix db connection

--- config/database.ts
+++ config/database.ts
@@ -4,8 +4,8 @@ export default ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapidb"),
-      user: env("DATABASE_USERNAME", ""),
+      database: env("DATABASE_NAME", "meadj"),
+      user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", ""),
       ssl: env.bool("DATABASE_SSL", false) && {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
