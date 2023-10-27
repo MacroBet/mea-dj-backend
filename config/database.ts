7e0b517f101e555214611d20f949afe80108c9c8
Marco Bellia <ing.bellia@gmail.com>
2023-10-27 17:25:24 +0200

    update db credential

--- config/database.ts
+++ config/database.ts
@@ -5,8 +5,8 @@ export default ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "meadj"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", ""),
+      user: env("DATABASE_USERNAME", "marco"),
+      password: env("DATABASE_PASSWORD", "marco"),
       ssl: env.bool("DATABASE_SSL", false) && {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
